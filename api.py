ee56c1da76ba9c9528d1c2e5e779c3d5b31b5253
bhavesh15184 <vipul15184@gmail.com>
2021-05-02 13:08:03 +0530

    Added option to get values in querystring

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import flask
 import json
 import prettytable
+from flask import request
 from prettytable import from_csv
 from SlotDetails import SlotDetails
 from cowin import getAvailibilityForRangePinCodes, writeSlotsToCSV, getPinCodes
@@ -15,15 +16,30 @@ def home():
 @app.route('/availibility', methods=['GET'])
 def availibility():
     INPUT_PINCODE_FILE = 'pincodes.csv'
+    age = 37
+    days = 7 
+
+    if 'pincode_file' in request.args:
+        INPUT_PINCODE_FILE = request.args['pincode_file']    
+    if 'age' in request.args:
+        age = int(request.args['age'])
+    if 'days' in request.args:
+        days = int(request.args['days'])
+
+    print("=====================================")
+    print("Running checks for PINCODE file: ", INPUT_PINCODE_FILE, ", DAYS: ", days, ", AGE: ", age)
     pincodes = getPinCodes(INPUT_PINCODE_FILE)
-    slots = getAvailibilityForRangePinCodes(7, 37, pincodes)
+    slots = getAvailibilityForRangePinCodes(days, age, pincodes)
 
-    OUTPUT_FILE = 'availibility.csv'
-    writeSlotsToCSV(slots, OUTPUT_FILE)
-    table = getTable(OUTPUT_FILE)
-    table.format = True
+    if len(slots) < 1:
+        return "No Slots Available!!"
+    else:
+        OUTPUT_FILE = 'availibility.csv'
+        writeSlotsToCSV(slots, OUTPUT_FILE)
+        table = getTable(OUTPUT_FILE)
+        table.format = True
 
-    return table.get_html_string(attributes={"name":"my_table", "class":"red_table"})
+        return table.get_html_string(attributes={"name":"my_table", "class":"red_table"})
 
 def getTable(OUTPUT_FILE):
     fp = open(OUTPUT_FILE, "r")
